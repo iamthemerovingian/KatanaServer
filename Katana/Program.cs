f304f9cd89ba688e7a6ff139c8ded1fdab456f95
SURFACEPRO\iamth <iamthemerovingian@hotmail.com>
2017-12-07 23:37:40 +0700

    Created a basic web server as a console application.

--- Katana/Program.cs
+++ Katana/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,27 @@ namespace Katana
     {
         static void Main(string[] args)
         {
+            string uri = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Server Started!!");
+                Console.ReadKey();
+                Console.WriteLine("Server Stopped :( ");
+
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+            //app.Run(ctx => 
+            //{
+            //    return ctx.Response.WriteAsync("Hello World");
+            //});
         }
     }
 }
