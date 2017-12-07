7babff7a67ee9360929ee3f7c247646bb4d76431
SURFACEPRO\iamth <iamthemerovingian@hotmail.com>
2017-12-08 00:05:46 +0700

    Explored how to construct your own custom middleware for OWIN. Pretty interesting stuff over here!! :D

--- Katana/Program.cs
+++ Katana/Program.cs
@@ -2,12 +2,15 @@
 using Owin;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Katana
 {
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
     class Program
     {
         static void Main(string[] args)
@@ -28,11 +31,41 @@ namespace Katana
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseWelcomePage();
-            //app.Run(ctx => 
-            //{
-            //    return ctx.Response.WriteAsync("Hello World");
-            //});
+            app.UseHelloWorld();
+            //app.Use<HellowWorldComponent>();
+            //app.UseWelcomePage();
+            ////app.Run(ctx => 
+            ////{
+            ////    return ctx.Response.WriteAsync("Hello World");
+            ////});
+        }
+    }
+
+    public static class HelloWorldAppBuilderExtensions
+    {
+        public static void UseHelloWorld(this IAppBuilder appBuilder)
+        {
+            appBuilder.Use<HellowWorldComponent>();
+        }
+    }
+
+    public class HellowWorldComponent
+    {
+        private readonly AppFunc TheNextMethodInThePipeline;
+
+        public HellowWorldComponent(AppFunc nextMethodInThePipeline)
+        {
+            TheNextMethodInThePipeline = nextMethodInThePipeline;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!!");
+            }
+            //return TheNextMethodInThePipeline(environment);
         }
     }
 }
